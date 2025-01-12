10c7bd641bc72c89c815063ff79f1ff7694231ad
Abinash Mahato <abiabiabi796@gmail.com>
2025-01-12 11:23:15 +0545

    EMS__setup:dynamic html contents

--- EventManagementSystem/src/main/java/com/EventManagementSystem/EventManagementSystem/controller/FrontendRender.java
+++ EventManagementSystem/src/main/java/com/EventManagementSystem/EventManagementSystem/controller/FrontendRender.java
@@ -1,12 +1,18 @@
 package com.EventManagementSystem.EventManagementSystem.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class FrontendRender {
     @GetMapping("/signup")
     public String login() {
         return "signup";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
 }
